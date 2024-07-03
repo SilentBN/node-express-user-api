9e8068c89d932a7ed70be6ebc2995148d4a5b406
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-03 19:46:15 -0400

    chore: Refactor GET endpoints for fetching users by ID and all users

--- api/server.js
+++ api/server.js
@@ -25,29 +25,39 @@ server.post("/api/users", async (req, res) => {
 });
 
 // New GET endpoint to fetch all users
-server.get("/api/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving the users" });
-  }
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
 });
 
 // New GET endpoint to fetch a user by ID
-server.get("/api/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Error retrieving the user" });
-  }
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The user information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
 });
 
 // DELETE endpoint to remove a user by ID
